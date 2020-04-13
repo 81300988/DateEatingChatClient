c46621d6b3fcc95249d7f9c1172f327de169fb64
hoanghaiha <hahh@dgroup.co>
2020-04-13 19:24:41 +0700

    first commit

---
+++ src/api/index.js
@@ -0,0 +1,31 @@
+import { w3cwebsocket as WebSocket } from "websocket";
+
+
+var socket = new WebSocket("ws://localhost:9090/ws");
+
+let connect = () => {
+  console.log("Attempting Connection...");
+
+  socket.onopen = () => {
+    console.log("Successfully Connected");
+  };
+
+  socket.onmessage = msg => {
+    console.log(msg);
+  };
+
+  socket.onclose = event => {
+    console.log("Socket Closed Connection: ", event);
+  };
+
+  socket.onerror = error => {
+    console.log("Socket Error: ", error);
+  };
+};
+
+let sendMsg = msg => {
+  console.log("sending msg: ", msg);
+  socket.send(msg);
+};
+
+export { connect, sendMsg };
